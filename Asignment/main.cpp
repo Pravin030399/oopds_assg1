dfe0ac755220f48b8ad4e062f6c13c1fbb91fe69
pravin <irpravin@gmail.com>
2019-02-11 18:15:14 +0800

    add the data into dictionary

--- Asignment/main.cpp
+++ Asignment/main.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <string>
 #include <map>
+#include <ctype.h>
 int main()
 {
     std::string filePath="D:/Pravin Assigment/Data/text/1.txt ";
@@ -17,16 +18,29 @@ int main()
     int lineNum(0);
     while(std::getline(buffer,temp,'\n')){ //access line by line
             std::string wordBuff="";
+            int Pos = 0;
             for(int i=0;i<temp.length();i++) //access character by character
                {
-                   if(temp[i] != ' ')
+                   if(temp[i] != ' ' && isalnum(temp[i]))
                    {
-                       wordBuff += temp[i];
+                       wordBuff += tolower(temp[i]);
                    }
                     else{
                         if(wordBuff.length() != 0)
-                            std::cout << wordBuff<<std::endl;
+                        {
+                            std::stringstream ss;
+                            if(textDataMap.find(wordBuff) == textDataMap.end()) // if wordbuff not exist
+                            {
+                                ss << "line:"  << lineNum << ",Pos:"<< Pos;
+                                textDataMap[wordBuff] = ss.str();
+                            }
+                            else{
+                                ss << "|line:" << lineNum << ",Pos:" << Pos;
+                                textDataMap[wordBuff] += ss.str();
+                            }
+                        }
                          wordBuff="";
+                         Pos=i;
                     }
                   //
                }
@@ -34,7 +48,11 @@ int main()
       lineNum++;
     }
     //std::cout << buffer.str() << std::endl;
-    std::cout<<lineNum<<std::endl;
+  //  std::cout<<lineNum<<std::endl;
 
+  for(std::map<std::string,std::string>::iterator it = textDataMap.begin(); it != textDataMap.end(); it++)
+  {
+      std::cout << it->first << " = " << it->second <<std::endl;
+  }
     return 0;
 }
