b21dc6799b3c357e3f8c7779f7ea98bb5b74b8bd
pravin <irpravin@gmail.com>
2019-02-11 17:47:41 +0800

    tokenize word by word

--- Asignment/main.cpp
+++ Asignment/main.cpp
@@ -2,16 +2,39 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <map>
 int main()
 {
     std::string filePath="D:/Pravin Assigment/Data/text/1.txt ";
     std::ifstream inputFile (filePath);
 
     std::stringstream buffer;
+    std::string temp;
     buffer << inputFile.rdbuf();
 
+    std::map<std::string,std::string> textDataMap;
 
+    int lineNum(0);
+    while(std::getline(buffer,temp,'\n')){ //access line by line
+            std::string wordBuff="";
+            for(int i=0;i<temp.length();i++) //access character by character
+               {
+                   if(temp[i] != ' ')
+                   {
+                       wordBuff += temp[i];
+                   }
+                    else{
+                        if(wordBuff.length() != 0)
+                            std::cout << wordBuff<<std::endl;
+                         wordBuff="";
+                    }
+                  //
+               }
+       // std::cout << std::endl;
+      lineNum++;
+    }
+    //std::cout << buffer.str() << std::endl;
+    std::cout<<lineNum<<std::endl;
 
-    std::cout << buffer.str() << std::endl;
     return 0;
 }
